f47d1996acc16ff66dc625a56628fa688dfaf624
rahmonovsaydullo <saydullo779@gmail.com>
2025-04-20 09:10:38 +0500

    I updfates index js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const userRouter = require('./routes/userRoutes')
 
 // Middlewares
 app.use(express.json())
 app.use(cors())
 
+// Calling routes
+app.use('/', userRouter)
+
+
+
 
 const PORT = 3030
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
