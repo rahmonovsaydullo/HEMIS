f8836fdfbc08cf08ae217eefcc0269b200b22a46
rahmonovsaydullo <saydullo779@gmail.com>
2025-04-21 12:49:14 +0500

    I setup Frontend

--- backend/controllers/subjectController.js
+++ backend/controllers/subjectController.js
@@ -2,12 +2,30 @@ const pool = require('../config/db')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-exports.getSubjects =  async (req,res) =>{
-   try {
-    const result = await pool.query("SELECT * FROM subjects")
-    res.status(200).json(result.rows)
-   } catch (error) {
-    console.log(error);
-    res.status(500).json({message: error})
-   }
+
+
+exports.addSubject = async (req, res) => {
+    try {
+        const { name } = req.body
+        const resultSubject = await pool.query(
+            'INSERT INTO subjects(name, teacher_id) VALUES($1, $2) RETURNING *',
+            [name, null]
+        )
+        res.status(201).json(resultSubject.rows[0])
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error })
+    }
 }
+
+exports.getSubjects = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM subjects")
+        res.status(200).json(result.rows)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error })
+    }
+}
+
+
--- backend/routes/subjectRoutes.js
+++ backend/routes/subjectRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getSubjects } = require('../controllers/subjectController')
+const { getSubjects, addSubject } = require('../controllers/subjectController')
 const subjectRouter = express.Router()
 
 
 subjectRouter.get('/subjects', getSubjects)
+subjectRouter.post('/subjects', addSubject)
 
 module.exports = subjectRouter
